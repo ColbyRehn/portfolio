b241e9e8866b278b45703e9bda38913d267d68c9
Colby Rehn <rehn.colby@gmail.com>
2023-01-28 19:47:29 +1100

    finished desktop Home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import blob from './blob.svg'
 import profile from './profile.svg'
 
 function App() {
   return (
   <div id="Home-page" class="Page-container">
-    <section id="Home-content-section">
-      <text class="Home-heading">Hello!<br /></text>
-      <text class="Home-heading">I'm Colby.<br /></text>
-      <text class="Home-subheading">A software engineer<br /></text>
-      <text class="Home-subheading">student from Australia.<br /></text>
-      <img id="Profile-picture" src={profile} alt="Colby Rehn"/>
+    <img id="Blob-bg" src={blob} alt="blob"/>
+    <section id="Home-section">
+        <img id="Profile-picture" src={profile} alt="Colby"/>
+        <text class="Home-heading">Hello!<br/></text>
+        <text class="Home-heading">I'm Colby.<br/></text>
+        <text class="Home-subheading">A software engineer<br/></text>
+        <text class="Home-subheading">student from Australia.</text>
     </section>
   </div>
   );
