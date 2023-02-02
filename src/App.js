bcf13d27b0f6df2c64e914260c818bc066a6e74a
Colby Rehn <rehn.colby@gmail.com>
2023-02-02 21:29:02 +1100

    menu scroll done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import home from './home.svg'
 import blob from './blob.svg'
@@ -13,6 +13,7 @@ import contactButton from './contact-button.svg'
 function App() {
   const [isCross, setIsCross] = useState(true);
   function burgerIcon(event) {
+    event.stopPropagation();
     var element = document.getElementById("Burger-icon");
     var menu = document.getElementById("Burger-column").style;
     setIsCross(!isCross);
@@ -27,32 +28,44 @@ function App() {
     }
   }
 
+  const homeSection = useRef(null);
+  const aboutSection = useRef(null);
+  const projectSection = useRef(null);
+  const contactSection = useRef(null);
+  function scrollTo(event, ref) {
+    event.stopPropagation();
+    if (ref.current) {
+      ref.current.scrollIntoView({top: ref.current.scrollHeight, behavior: "smooth"});
+      burgerIcon(event);
+    }
+  }
+
   return (
     <body style={{display: "block"}}>
-      <div id="Burger-menu-button" onClick={burgerIcon}>
+      <div id="Burger-menu-button" onClick={(e) => burgerIcon(e)}>
         <img id="Burger-icon" src={burger} alt="burger-icon" />
       </div>
       <div id="Burger-column">
-        <div style={{display: "block"}}>
-          <div class="Burger-page-button">
+        <div style={{flexDirection: "column"}}>
+          <div class="Burger-page-button" onClick={(e) => scrollTo(e, homeSection)}>
             <img src={homeButton} alt="burger-icon" />
             <text class="Burger-button-text">Home</text>
           </div>
-          <div class="Burger-page-button">
+          <div class="Burger-page-button" onClick={(e) => scrollTo(e, aboutSection)}>
             <img src={profileButton} alt="profile-icon" />
             <text class="Burger-button-text">Profile</text>
           </div>
-          <div class="Burger-page-button">
+          <div class="Burger-page-button" onClick={(e) => scrollTo(e, projectSection)}>
             <img src={projectButton} alt="project-icon" />
             <text class="Burger-button-text">Projects</text>
           </div>
-          <div class="Burger-page-button">
+          <div class="Burger-page-button" onClick={(e) => scrollTo(e, contactSection)}>
             <img src={contactButton} alt="contact-icon" />
             <text class="Burger-button-text">Contact</text>
           </div>
         </div>
       </div>
-      <div id="Home-page" class="Page-container">
+      <div ref={homeSection} id="Home-page" class="Page-container">
         <section class="Header-section">
           <img id="Home-icon" src={home} alt="home-icon"/>
           <text class="Icon-name">HOME<br/></text>
@@ -66,8 +79,11 @@ function App() {
           <text class="Home-subheading">student from Australia.</text>
         </section>
       </div>
-      <div id="About-page" class="Page-container">
-
+      <div ref={aboutSection} id="About-page" class="Page-container">
+      </div>
+      <div ref={projectSection} id="Project-page" class="Page-container">
+      </div>
+      <div ref={contactSection} id="Contact-page" class="Page-container">
       </div>
     </body>
   );
